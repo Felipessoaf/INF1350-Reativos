f92fb86700d3e585ac9f7d1519ae0f07a3cc3692
guibizzo <guibizzo@gmail.com>
2020-03-14 16:09:35 -0300

    Ex3 funcionando

--- ex03/ex03.ino
+++ ex03/ex03.ino
@@ -1,15 +1,14 @@
 #include "pindefs.h"
 int ledOn = 0;
+int canBlink = 1;
 unsigned long previousMillis = 0;
 unsigned long interval = 500;
-unsigned long botao = 0;
-unsigned long interBot = 400;
+unsigned long ledInterval = 500;
+unsigned long old = 0;
 
 unsigned long minimo = 125;
 unsigned long maximo = 1000;
 
-//teste
-//outro teste
 
 void setup() {
   pinMode(LED1, OUTPUT);;
@@ -21,29 +20,24 @@ void setup() {
 
 void loop () {
   unsigned long currentMillis = millis();
-  unsigned long currentBotao = millis();
-  unsigned long aux;
-  if (currentMillis - previousMillis >= interval) {
-      previousMillis = currentMillis;
-      ledOn = !ledOn;
-      digitalWrite(LED1, ledOn);
 
-      if(currentBotao - botao >= interBot){
-        if (digitalRead(KEY1) == LOW and interval > minimo) {
-        interval = interval/2;
-      }
-        if(digitalRead(KEY2) == LOW and interval < maximo){
-          interval = interval*2;
-      }
-        if(digitalRead(KEY3) == LOW){
-          if(interval != 0){
-            aux = interval;
-            interval = 0;
-          }
-          if(interval == 0){
-            interval = aux;
-          }
-      }
+  if (currentMillis >= old + ledInterval and canBlink) { // hora de piscar?
+    old = currentMillis;
+    ledOn = !ledOn;
+    digitalWrite(LED1, ledOn);
+  }
+
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+
+    if (digitalRead(KEY1) == LOW and ledInterval > minimo) {
+      ledInterval = ledInterval / 2;
+    }
+    if (digitalRead(KEY2) == LOW and ledInterval < maximo) {
+      ledInterval = ledInterval * 2;
+    }
+    if (digitalRead(KEY3) == LOW) {
+      canBlink = !canBlink;
     }
   }
 }
