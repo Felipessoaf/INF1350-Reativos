4c0572d43547dd2ada4f7a4709d9c40022731d75
guibizzo <guibizzo@gmail.com>
2020-03-20 18:07:46 -0300

    [wip] - timer_event

--- Exercicios/Arduino2 - Event Driven/eventdriven/eventdriven.ino
+++ Exercicios/Arduino2 - Event Driven/eventdriven/eventdriven.ino
@@ -3,8 +3,8 @@
 #include "pindefs.h"
 
 int timers[3];
-int timersUsed[3] = {0,0,0};
-int buttons[3] = {-1, -1, -1};
+int timersUsed[3] = {0, 0, 0};
+int buttons[3] = { -1, -1, -1};
 
 unsigned long previousMillis = 0;
 unsigned long interval = 500;
@@ -12,70 +12,74 @@ unsigned long interval = 500;
 /* Funcoes de registro: */
 void button_listen(int pin)
 {
-    int slot = -1;
-    
-    // "pin" passado deve gerar notificacoes
-    for(int i = 0; i < 3; i++)
+  int slot = -1;
+
+  // "pin" passado deve gerar notificacoes
+  for (int i = 0; i < 3; i++)
+  {
+    if (buttons[i] != pin && buttons[i] < 0)
     {
-        if(buttons[i] != pin && buttons[i] < 0)
-        {
-            buttons[i] = pin;
-            pinMode(pin, INPUT_PULLUP);
-        }
-        
-        if(buttons[i] != pin && buttons[i] < 0)
-        {
-            buttons[i] = pin;
-            pinMode(pin, INPUT_PULLUP);
-        }
+      buttons[i] = pin;
+      pinMode(pin, INPUT_PULLUP);
     }
+
+    if (buttons[i] != pin && buttons[i] < 0)
+    {
+      buttons[i] = pin;
+      pinMode(pin, INPUT_PULLUP);
+    }
+  }
 }
 
 void timer_set (int t, int ms)
 {
-    // timer t deve expirar apos "ms" milisegundos
-    // timer so dispara uma vez
-    timers[t] = ms;
-    timersUsed[t] = 1;
+  // timer t deve expirar apos "ms" milisegundos
+  // timer so dispara uma vez
+  timers[t] = ms;
+  timersUsed[t] = 1;
 }
 
 /* Callbacks  - definidas em app.h*/
 
 // notifica que "pin" mudou para "v"
-void button_changed (int pin, int v);  
+void button_changed (int pin, int v);
 // notifica que o timer expirou
 void timer_expired (void);
 
 /* Programa principal: */
-void setup () 
+void setup ()
 {
-    // inicializacao da API
+  // inicializacao da API
 
-    // inicializacao do usuario
-    appinit();                 
+  // inicializacao do usuario
+  appinit();
 }
 
-void loop (dt) 
+void loop ()
 {
-    // detecta novos eventos
-    if()
+  unsigned long currentMillis = millis();
+  // detecta novos eventos
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;
+    if ()
     {
-        // notifica o usuario
-        button_changed(); 
+      // notifica o usuario
+      button_changed();
     }
 
-    
+
     // detecta novos eventos
-    for(int i = 0; i < 3; i++)
+    for (int i = 0; i < 3; i++)
     {
-        //atualiza o tempo 
-        timers[i] -= dt;
-        if(timers[i] <= 0 and timersUsed[i] == 1)
-        {
-            // notifica o usuario
-            timer_expired(i);
-            timersUsed[i] = 0
-        } 
+      //atualiza o tempo
+      timers[i] -= dt;
+      if (timers[i] <= 0 and timersUsed[i] == 1)
+      {
+        // notifica o usuario
+        timer_expired(i);
+        timersUsed[i] = 0
+      }
     }
+  }
 
 }
