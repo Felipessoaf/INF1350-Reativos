cfea6fec598094c8dd98f12514e91f6dabfdef88
Felipe Pessoa <felipessoaf@hotmail.com>
2020-03-20 19:41:19 -0300

    [wip] app2

--- Exercicios/Arduino2 - Event Driven/eventdriven_2/app2.ino
+++ Exercicios/Arduino2 - Event Driven/eventdriven_2/app2.ino
@@ -3,25 +3,83 @@
 #include "pindefs.h"
 
 int state = LOW;
+int canBlink = 1;
+unsigned long ledInterval = 500;
+
+unsigned long btnInterval = 500;
+
+unsigned long oldMillisBtn1 = 0;
+unsigned long oldMillisBtn2 = 0;
+
+unsigned long ledMinimo = 125;
+unsigned long ledMaximo = 1000;
 
 void appinit()
 {
-    button_listen(KEY1);
-    timer_set(1, 1000);
-    digitalWrite(LED1, HIGH);
-    digitalWrite(LED2, HIGH);
-    digitalWrite(LED3, HIGH);
+  button_listen(KEY1);
+  button_listen(KEY2);
+  button_listen(KEY3);
+
+  timer_set(0, ledInterval);
+
+  digitalWrite(LED1, HIGH);
+  digitalWrite(LED2, HIGH);
+  digitalWrite(LED3, HIGH);
 }
 
-void button_changed (int pin) 
+void button_changed (int pin)
 {
-    digitalWrite(LED1, LOW);
-    exit(0);
+  if (pin == KEY1)
+  {
+    Serial.println("btn 1");
+    if (millis() - oldMillisBtn2 < btnInterval)
+    {
+      Serial.println("btn 1 && 2");
+      canBlink = !canBlink;
+    }
+    else
+    {
+      ledInterval /= 2;
+
+      if (ledInterval < ledMinimo)
+      {
+        ledInterval = ledMinimo;
+      }
+    }
+
+    oldMillisBtn1 = millis();
+  }
+  else if (pin == KEY2)
+  {
+    Serial.println("btn 2");
+    if (millis() - oldMillisBtn1 < btnInterval)
+    {
+      Serial.println("btn 2 && 1");
+      canBlink = !canBlink;
+    }
+    else
+    {
+      ledInterval *= 2;
+
+      if (ledInterval > ledMaximo)
+      {
+        ledInterval = ledMaximo;
+      }
+    }
+
+    oldMillisBtn2 = millis();
+  }
 }
 
-void timer_expired (int t) 
+void timer_expired (int t)
 {
-    state = !state;
-    digitalWrite (LED1, state);
-    timer_set (1, 1000);
+  if (t == 0)
+  {
+    if (canBlink)
+    {
+      state = !state;
+      digitalWrite (LED1, state);
+    }
+    timer_set (1, ledInterval);
+  }
 }
