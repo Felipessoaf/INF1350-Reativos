3d2ee5d2ca28a6196fcd20cbf1579e80d1275fe9
Felipe Pessoa <felipessoaf@hotmail.com>
2020-03-20 17:54:14 -0300

    [wip] event

--- Exercicios/Arduino2 - Event Driven/eventdriven/eventdriven.ino
+++ Exercicios/Arduino2 - Event Driven/eventdriven/eventdriven.ino
@@ -3,6 +3,7 @@
 #include "pindefs.h"
 
 int timers[3];
+int timersUsed[3] = {0,0,0};
 int buttons[3] = {-1, -1, -1};
 
 unsigned long previousMillis = 0;
@@ -11,9 +12,17 @@ unsigned long interval = 500;
 /* Funcoes de registro: */
 void button_listen(int pin)
 {
+    int slot = -1;
+    
     // "pin" passado deve gerar notificacoes
     for(int i = 0; i < 3; i++)
     {
+        if(buttons[i] != pin && buttons[i] < 0)
+        {
+            buttons[i] = pin;
+            pinMode(pin, INPUT_PULLUP);
+        }
+        
         if(buttons[i] != pin && buttons[i] < 0)
         {
             buttons[i] = pin;
@@ -27,6 +36,7 @@ void timer_set (int t, int ms)
     // timer t deve expirar apos "ms" milisegundos
     // timer so dispara uma vez
     timers[t] = ms;
+    timersUsed[t] = 1;
 }
 
 /* Callbacks  - definidas em app.h*/
@@ -45,7 +55,7 @@ void setup ()
     appinit();                 
 }
 
-void loop () 
+void loop (dt) 
 {
     // detecta novos eventos
     if()
@@ -54,8 +64,18 @@ void loop ()
         button_changed(); 
     }
 
+    
     // detecta novos eventos
+    for(int i = 0; i < 3; i++)
+    {
+        //atualiza o tempo 
+        timers[i] -= dt;
+        if(timers[i] <= 0 and timersUsed[i] == 1)
+        {
+            // notifica o usuario
+            timer_expired(i);
+            timersUsed[i] = 0
+        } 
+    }
 
-    // notifica o usuario
-    timer_expired();
 }
