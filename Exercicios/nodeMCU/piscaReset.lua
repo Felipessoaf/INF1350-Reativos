bc37295b74838549fac102ed4a03d5f101dabecd
Felipe Pessoa <felipessoaf@hotmail.com>
2020-05-05 18:46:45 -0300

    [wip] pisca reset e pisca velocidade

---
+++ Exercicios/nodeMCU/piscaReset.lua
@@ -0,0 +1,43 @@
+local led1 = 0
+local led2 = 6
+local sw1 = 3
+local sw2 = 4
+
+local function disparapiscapisca (led, tempo)
+    local apagado = true
+
+    local function piscapisca(timer)
+        if apagado then
+        gpio.write(led, gpio.HIGH);
+        else
+        gpio.write(led, gpio.LOW);
+        end
+        apagado = not apagado
+    end
+
+    -- coloca o pino dos leds em modo de saida
+    gpio.mode(led, gpio.OUTPUT)
+    -- apaga o led
+    gpio.write(led, gpio.LOW);
+    
+    local mytimer = tmr.create()
+    mytimer:register(tempo, tmr.ALARM_AUTO, piscapisca)
+    mytimer:start()
+
+    return function ()
+        print("piscaReset")
+        if mytimer:state() then
+            print("stop")
+            mytimer:stop()
+        else
+            print("start")
+            mytimer:start()
+        end
+    end
+end
+
+local piscaReset1 = disparapiscapisca (led1, 500)
+gpio.trig(sw1, "down", piscaReset1)
+
+local piscaReset2 = disparapiscapisca (led2, 750)
+gpio.trig(sw2, "down", piscaReset2)
---
+++ Exercicios/nodeMCU/piscaVelocidade.lua
@@ -0,0 +1,47 @@
+local led1 = 0
+local sw1 = 3
+local sw2 = 4
+
+local function disparapiscapisca (led, tempo)
+    local apagado = true
+
+    local function piscapisca(timer)
+        if apagado then
+        gpio.write(led, gpio.HIGH);
+        else
+        gpio.write(led, gpio.LOW);
+        end
+        apagado = not apagado
+    end
+
+    -- coloca o pino dos leds em modo de saida
+    gpio.mode(led, gpio.OUTPUT)
+    -- apaga o led
+    gpio.write(led, gpio.LOW);
+    
+    local mytimer = tmr.create()
+    mytimer:register(tempo, tmr.ALARM_AUTO, piscapisca)
+    mytimer:start()
+
+    return function ()
+        mytimer:unregister()
+        tempo = tempo/2
+        if tempo < 50 then
+            tempo = 50
+        end
+        mytimer:register(tempo, tmr.ALARM_AUTO, piscapisca)
+    end,
+    function ()
+        mytimer:unregister()
+        tempo = tempo*2
+        if tempo > 2000 then
+            tempo = 2000
+        end
+        mytimer:register(tempo, tmr.ALARM_AUTO, piscapisca)
+    end
+
+end
+
+local diminuiTempo,aumentaTempo = disparapiscapisca (led1, 500)
+gpio.trig(sw1, "down", diminuiTempo)
+gpio.trig(sw2, "down", aumentaTempo)
