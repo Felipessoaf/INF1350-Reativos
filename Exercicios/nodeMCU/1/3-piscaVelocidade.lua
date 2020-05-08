531a32c358432bbe86cd549ff8c705735d7078bf
Felipe Pessoa <felipessoaf@hotmail.com>
2020-05-08 00:05:35 -0300

    pisca velocidade done

--- Exercicios/nodeMCU/1/3-piscaVelocidade.lua
+++ Exercicios/nodeMCU/1/3-piscaVelocidade.lua
@@ -1,3 +1,6 @@
+--Nome: Felipe Pessoa e Guilherme Bizzo
+--Matricula: 1411716 e 1710563
+
 local led1 = 0
 local sw1 = 3
 local sw2 = 4
@@ -7,24 +10,37 @@ gpio.mode(sw2,gpio.INT,gpio.PULLUP)
 
 local function disparapiscapisca (led, tempo)
     local apagado = true
+    local key1Pressed = false
+    local key2Pressed = false
+    local dbtmr = tmr.create()
+    
+    local mytimer = tmr.create()
 
     local function piscapisca(timer)
         if apagado then
-        gpio.write(led, gpio.HIGH);
+            gpio.write(led, gpio.HIGH);
         else
-        gpio.write(led, gpio.LOW);
+            gpio.write(led, gpio.LOW);
         end
-        apagado = not apagado
+            apagado = not apagado
     end
+
+    local function checkStopAll(key)
+        if key then
+            gpio.trig(sw1)
+            gpio.trig(sw2)
+            mytimer:unregister()
+            gpio.write(led, gpio.LOW);
+        end
+    end
+    
+    mytimer:register(tempo, tmr.ALARM_AUTO, piscapisca)
+    mytimer:start()
 
     -- coloca o pino dos leds em modo de saida
     gpio.mode(led, gpio.OUTPUT)
     -- apaga o led
     gpio.write(led, gpio.LOW);
-    
-    local mytimer = tmr.create()
-    mytimer:register(tempo, tmr.ALARM_AUTO, piscapisca)
-    mytimer:start()
 
     return function ()
         mytimer:unregister()
@@ -34,6 +50,15 @@ local function disparapiscapisca (led, tempo)
         end
         mytimer:register(tempo, tmr.ALARM_AUTO, piscapisca)
         mytimer:start()
+
+        key1Pressed = true
+        checkStopAll(key2Pressed)
+
+        dbtmr:unregister()
+        dbtmr:register(500, tmr.ALARM_SINGLE, function()
+            key1Pressed = false
+        end)
+        dbtmr:start()        
     end,
     function ()
         mytimer:unregister()
@@ -43,6 +68,15 @@ local function disparapiscapisca (led, tempo)
         end
         mytimer:register(tempo, tmr.ALARM_AUTO, piscapisca)
         mytimer:start()
+
+        key2Pressed = true
+        checkStopAll(key1Pressed)
+
+        dbtmr:unregister()
+        dbtmr:register(500, tmr.ALARM_SINGLE, function()
+            key2Pressed = false
+        end)
+        dbtmr:start()  
     end
 
 end
