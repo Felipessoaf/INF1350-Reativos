b6fe1083d00a299c4b6779d78706b5627b680b99
guibizzo <guibizzo@gmail.com>
2020-05-09 20:21:30 -0300

    Modificações comentadas apenas

--- Projs/MiniProjLove/main.lua
+++ Projs/MiniProjLove/main.lua
@@ -3,13 +3,15 @@
 
 local invaders = {}
 local player
+local shot
+local GameState = 0 -- 0, se estiver no menu; 1, se o jogo estiver em andamento; 2, se o jogador vencer ou perder
 
-local function newInvader (x, y, color)
+local function newInvader (x, y, color, vel)
     local tam = 40
 
     return {
         update = function (dt)
-            
+        
         end,
 
         draw = function ()
@@ -17,8 +19,20 @@ local function newInvader (x, y, color)
             love.graphics.rectangle("fill", x, y, tam, tam/4)
             love.graphics.setColor(1, 1, 1)
             love.graphics.rectangle("line", x, y, tam, tam/4)
-        end
+        end,
+        
+        --affected = function (pos)
+            --if pos>x and pos<x+50 then
+            -- atingiu o invader --
+                --return true
+            --else
+            -- errou o tiro --
+                --return false
+            --end
+        --end
     }
+    
+    
     end
 
 local function newplayer ()
@@ -40,9 +54,35 @@ local function newplayer ()
     }
 end
 
+--local function shot()
+--  local screenWidth, screenHeight = love.graphics.getDimensions()
+--  local width, height = 20, 40
+--  local x, y = (screenWidth - width)/2, screenHeight - height + 20 
+
+--  return {
+--      update = function (dt)
+          
+--      end,
+      
+--      draw = function ()
+--          love.graphics.setColor(0, 1, 34/255)
+--          love.graphics.rectangle("fill", x, y, width, height)
+--          love.graphics.setColor(1, 1, 1)
+--         love.graphics.rectangle("line", x, y, width, height)
+--      end
+--  }
+--end
+
 function love.keypressed (key)
     if key == 'space' then
-        --atira
+      --atira--
+      --shot.draw()
+    end
+    if key == 'a' then
+      --player.x -= 10
+    end
+    if key == 'd' then
+      --player.x += 10
     end
 end
 
@@ -70,7 +110,8 @@ function love.draw()
 
     for i,invader in pairs(invaders) do
         invader.draw()
-    end    
+    end
+    
 end
 
 function love.update(dt)
