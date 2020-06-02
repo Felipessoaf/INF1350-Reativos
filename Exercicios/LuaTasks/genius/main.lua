9a2fb085cd337780a0f8cf6c7b21040a7002ec0e
Felipe Pessoa <felipessoaf@hotmail.com>
2020-06-02 17:35:58 -0300

    ex 1.1

--- Exercicios/LuaTasks/genius/main.lua
+++ Exercicios/LuaTasks/genius/main.lua
@@ -2,7 +2,7 @@
 -- Example:
 -- genius:
 -- generate random sequence of numbers
--- wait for user to pree keys and check whether sequence is the same
+-- wait for user to press keys and check whether sequence is the same
 -- if user presses "q" at any time quit program
 -- if user presses "n" during sequence generation, start new sequence
 -- Parallel API example
@@ -35,76 +35,88 @@ local switches = {}
 local numswitches = 2
 
 local function indisk (disk, mx, my)
-  return math.sqrt((mx-disk.x)^2 + (my-disk.y)^2) < disk.r
+    return math.sqrt((mx-disk.x)^2 + (my-disk.y)^2) < disk.r
 end
 
 local function showswitch (i)
-  switches[i].colors =colors[i][2]
-  await_ms(300)
-  switches[i].colors = colors[i][1]
+    switches[i].colors =colors[i][2]
+    await_ms(300)
+    switches[i].colors = colors[i][1]
 end
 
 
 function love.load()
-  -- inicia janela com botÃµes
+    -- inicia janela com botoes
 
-  love.window.setMode(SIZE,SIZE)
-  love.graphics.setBackgroundColor(0,0,0)
+    love.window.setMode(SIZE,SIZE)
+    love.graphics.setBackgroundColor(0,0,0)
 
-  for i = 1, numswitches do
-    switches[i] = {colors = colors[i][1], x = i*SIZE/3, y = SIZE/2, r = SIZE/8}
-  end
-  love.math.setRandomSeed (os.time())
+    for i = 1, numswitches do
+        switches[i] = {colors = colors[i][1], x = i*SIZE/3, y = SIZE/2, r = SIZE/8}
+    end
+    love.math.setRandomSeed (os.time())
 
-  -- "main"
+    -- "main"
 
-  local main_task = tasks.task_t:new(
-          function()
+    local main_task = tasks.task_t:new(
+        function()
             while true do  
-              print("novo jogo!")
-              await_ms(500)
-              for i = 1, tamseq do
-                seq[i] = love.math.random(1,numswitches)
-                print(seq[i])
-                showswitch(seq[i])
-                await_ms(500)
-              end
-              print("-- agora usuario repete --")
-              for i = 1, tamseq do
-                local key = await ("switchpressed")
-                showswitch(key)
-                if key ~= seq[i] then
-                  print ("errou!")
-                  break
-                end
-              end
+                local exec = tasks.par_or(function() 
+                    print("novo jogo!")
+                    await_ms(500)
+                    for i = 1, tamseq do
+                        seq[i] = love.math.random(1,numswitches)
+                        print(seq[i])
+                        showswitch(seq[i])
+                        await_ms(500)
+                    end
+                    print("-- agora usuario repete --")
+                    for i = 1, tamseq do
+                        local key = await ("switchpressed")
+                        showswitch(key)
+                        if key ~= seq[i] then
+                            print ("errou!")
+                            break
+                        end
+                    end
+                end, 
+                    function() 
+                        local key = await("keypressed")
+                        while key ~= "q" do
+                            key = await("keypressed")
+                        end
+                        print ("sair do jogo")
+                        love.event.quit()
+                    end
+                )
+                exec()                
             end
-          end
-        )
+        end
+    )
 	main_task()
 end
 
 function love.update(dt)
-  update_time(dt * 1000)
+    update_time(dt * 1000)
 end
 
 function love.draw ()
-  for i = 1, numswitches do
-    love.graphics.setColor(switches[i].colors[1], switches[i].colors[2], switches[i].colors[3])
-    love.graphics.circle ("fill", switches[i].x, switches[i].y, switches[i].r, 64)
-  end
+    for i = 1, numswitches do
+        love.graphics.setColor(switches[i].colors[1], switches[i].colors[2], switches[i].colors[3])
+        love.graphics.circle ("fill", switches[i].x, switches[i].y, switches[i].r, 64)
+    end
 end
 
 function love.mousepressed (mx, my)
-  for i = 1, numswitches do
-    if indisk (switches[i], mx, my) then
-      print ("switchpressed", i)
-      emit ("switchpressed", i)
+    for i = 1, numswitches do
+        if indisk (switches[i], mx, my) then
+            print ("switchpressed", i)
+            emit ("switchpressed", i)
+        end
     end
-  end
 end
 
 function love.keypressed(key, scancode)
-  print ("keypressed", key)
-  emit("keypressed", key)
+    print ("keypressed", key)
+    emit("keypressed", key)
 end
