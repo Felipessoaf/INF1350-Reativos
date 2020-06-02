b17b179880fccec5ebc03bf02a3b06fd35c3b5fb
Felipe Pessoa <felipessoaf@hotmail.com>
2020-06-02 18:17:10 -0300

    lua task ex2

--- Exercicios/LuaTasks/blinkVel/main.lua
+++ Exercicios/LuaTasks/blinkVel/main.lua
@@ -10,110 +10,58 @@ local update_time = tasks.update_time
 local par_or = tasks.par_or
 local par_and = tasks.par_and
 
+local led1 = true
 local interval = 500
-
 local SIZE = 400
-local colors = {}
-colors[1] = {
-  {0.6, 0, 0},
-  {1,0,0}
-}
-colors[2] = {
-  {0.35,0.4,0.2},
-  {0.5,1,0}
-}
-local switches = {}
-local numswitches = 2
-
-local function showswitch (i)
-    switches[i].colors =colors[i][2]
-    await_ms(300)
-    switches[i].colors = colors[i][1]
-end
-
 
 function love.load()
-    -- inicia janela com botoes
-
     love.window.setMode(SIZE,SIZE)
     love.graphics.setBackgroundColor(0,0,0)
 
-    for i = 1, numswitches do
-        switches[i] = {colors = colors[i][1], x = i*SIZE/3, y = SIZE/2, r = SIZE/8}
+    local function blink()
+        while true do
+            tasks.await_ms(interval)
+            led1 = not led1
+        end
     end
-
+    
     local main_task = tasks.task_t:new(
         function()
-            while true do  
-                local game = tasks.par_or(function() 
-                    print("novo jogo!")
-                    await_ms(500)
-
-                    local sequence = tasks.par_or(function()
-                        for i = 1, tamseq do
-                            seq[i] = love.math.random(1,numswitches)
-                            print(seq[i])
-                            showswitch(seq[i])
-                            await_ms(500)
-                        end
-
-                        print("-- agora usuario repete --")
-                        for i = 1, tamseq do
-                            local key = await ("switchpressed")
-                            showswitch(key)
-                            if key ~= seq[i] then
-                                print ("errou!")
-                                break
-                            end
-                        end
-                    end, 
-                        function() 
-                            local key = await("keypressed")
-                            while key ~= "n" do
-                                key = await("keypressed")
-                            end
-                            print("sequence restart")
-                        end
-                    )
-                    sequence()                    
-                end, 
+            while true do
+                local exec = tasks.par_or(blink, 
                     function() 
-                        local key = await("keypressed")
-                        while key ~= "q" do
-                            key = await("keypressed")
+                        local key = await ("keyreleased")
+                        if key == "h" then
+                            print("speed up")
+                            interval = interval/2
+                            if interval < 100 then
+                                interval = 100
+                            end
+                        elseif key == "l" then
+                            print("speed down")
+                            interval = interval*2
+                            if interval > 2000 then
+                                interval = 2000
+                            end
                         end
-                        print ("sair do jogo")
-                        love.event.quit()
-                    end
-                )
-                game()                
+                    end)
+                exec()
             end
         end
     )
-	main_task()
+    main_task()
 end
 
 function love.update(dt)
     update_time(dt * 1000)
 end
 
-function love.draw ()
-    for i = 1, numswitches do
-        love.graphics.setColor(switches[i].colors[1], switches[i].colors[2], switches[i].colors[3])
-        love.graphics.circle ("fill", switches[i].x, switches[i].y, switches[i].r, 64)
-    end
+function love.keyreleased(key)
+    print("keyreleased", key)
+    emit("keyreleased", key)
 end
 
-function love.mousepressed (mx, my)
-    for i = 1, numswitches do
-        if indisk (switches[i], mx, my) then
-            print ("switchpressed", i)
-            emit ("switchpressed", i)
-        end
-    end
-end
-
-function love.keypressed(key, scancode)
-    print ("keypressed", key)
-    emit("keypressed", key)
+function love.draw()
+    love.graphics.setColor(1, 0, 0)
+    love.graphics.circle(led1 and "fill" or "line", SIZE/2, SIZE/2, 100, 100)
 end
